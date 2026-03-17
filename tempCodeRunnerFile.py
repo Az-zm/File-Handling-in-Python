00a60c0505e478393e9921a73fa7f47e9de5dfe5
Az-zm <azmhussain056@gmail.com>
2026-03-17 12:23:16 +0530

    initial commit

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+with open("output.txt","at") as fh:
+#     fh.write(input("Enter text to write to the file : "))
+#     print("Data successfully written to output.txt")
+#     fh.write("\n")
+#     fh.write(input("Enter additional text to append : "))
+#     print("Data successfully appended.")
