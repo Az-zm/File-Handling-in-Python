beef6ee014931d8c23af1485dc66782e2920d9be
Azm Hussain <azmhussain056@gmail.com>
2026-05-17 06:24:31 +0530

    modified commit

--- Task2.py
+++ Task2.py
@@ -1,9 +1,12 @@
+# a mode is used to create a non-existing file and write data to it. If the file already exists, it will be overwritten.
 with open("output.txt","at") as fh:
+    # using user input to append data and write to the file
     fh.write(input("Enter text to write to the file : "))
     print("Data successfully written to output.txt")
     fh.write("\n")
     fh.write(input("Enter additional text to append : "))
     print("Data successfully appended.")
+# reading the content of the file and printing it to the console
 with open("output.txt","rt") as fh :
     content = fh.read()
     print(f"Final content of output.txt : {content}")     
